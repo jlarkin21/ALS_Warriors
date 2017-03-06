68950733de524969bef4c6dc3eeedc9d4a1f7b9a
jlarkin21 <jlarkin9@gmail.com>
2017-03-06 01:22:26 -0500

    Initial Commit

---
+++ ALS_Warrior/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALS_Warrior
+//
+//  Created by Justin Larkin on 3/6/17.
+//  Copyright © 2017 Justin Larkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
